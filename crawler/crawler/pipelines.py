734346dc0266337155e9a60a1384b5e6a884a09c
ywjjj <38972404+ywjjj@users.noreply.github.com>
2018-05-15 21:36:49 +0800

    crawler projects based on Scrapy

---
+++ crawler/crawler/pipelines.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+import itertools
+
+
+class CrawlerPipeline(object):
+    def __init__(self):
+        self.csvwriter = csv.writer(open('E:/dataset/ml-latest/scores.csv', 'w', newline=''), delimiter=',')
+
+    def process_item(self, item, spider):
+        self.csvwriter.writerow((str(item['imdbId']), item['score']))
+        print(str(item['imdbId']), '已经存入')
+        return item
---
+++ crawler/crawler/spiders/moviecrawler.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import scrapy
+from crawler.items import CrawlerItem
+from scrapy.http import Request
+import pandas as pd
+
+
+class MovieSpider(scrapy.Spider):
+    name = 'crawler'
+    allowed_domains = ['imdb.com']
+    base_url = 'https://www.imdb.com/title/tt'
+    links = pd.read_csv('E:/dataset/ml-latest/links.csv', usecols=[1], dtype={'imdbId': str})
+    imdbdict = links.imdbId.tolist()
+
+    def start_requests(self):
+        for i in self.imdbdict:
+            url = self.base_url + i
+            yield Request(url, self.parse)
+
+    def parse(self, response):
+        items = CrawlerItem()
+        imdbId = str(response.url)[-8:-1]
+        items['imdbId'] = str(imdbId)
+        score = response.xpath('//span[@itemprop="ratingValue"]/text()').extract()[0]
+        items['score'] = score
+        return items
